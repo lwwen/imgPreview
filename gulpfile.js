0c80677d17cadf3101ccc9def30dfe6991c6a9fe
lwwen <798977338@qq.com>
2016-12-16 17:21:30 +0800

    when remove goto change

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-    minify=require('gulp-minify-css');
-gulp.task('script', function() {
-    return gulp.src('dist/css/*.css')      //压缩的文件
-    .pipe(minify())
-        .pipe(gulp.dest('dist/min/'))   //输出文件夹
-});
